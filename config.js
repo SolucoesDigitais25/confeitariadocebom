af2e7cdd4d4885fb5cc8869a53084081fc6dbcfe
SolucoesDigitais25 <leo.solucoes25@gmail.com>
2025-08-05 21:10:14 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -109,7 +109,7 @@ const TIMEZONE = 'America/Sao_Paulo';
  * const PRELOADER_LOGO_URL = 'https://dropbox.com/s/abc/logo.png?dl=0';
  * const PRELOADER_LOGO_URL = ''; // Sem logo
  */
-const PRELOADER_LOGO_URL = 'https://drive.google.com/file/d/1w5D-K4N_Vonw9zLT-fuanhHxZLHH__QP/view?usp=drive_link'; // Adicione a URL do seu logo aqui
+const PRELOADER_LOGO_URL = 'https://drive.google.com/file/d/1y2AZiIjurFmeBFR35RFVOEhW7nKhVmtc/view?usp=drive_link'; // Adicione a URL do seu logo aqui
 
 /**
  * COR DO PRELOADER:
